d4cfb4d2b2d8fb04cf558ed5541a883ce9b414d9
Eduart Luis Doria Acevedo <eduart.doria@aossas.com>
2019-09-26 16:16:47 -0500

    Variable de entorno MONGO DB

--- server/config/config.js
+++ server/config/config.js
@@ -15,7 +15,8 @@ let urlDB;
 // if (process.env.NODE_ENV === 'dev') {
 //     urlDB = 'mongodb://localhost:27017/cafe';
 // } else {
-urlDB = 'mongodb+srv://udemy:SaIMe923JGUUaNr9@cluster0-rfjbz.mongodb.net/cafe';
+urlDB = process.env.MOGODB_URL;
+//'mongodb+srv://udemy:SaIMe923JGUUaNr9@cluster0-rfjbz.mongodb.net/cafe';
 // }
 
 process.env.URL_DB = urlDB;
